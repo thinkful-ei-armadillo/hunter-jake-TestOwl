733b5cd12d13df300500620b45c2a2fffd4493d2
JuiceboxJones <jderhalli@gmail.com>
2019-02-05 15:16:23 -0800

    displaying users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import UserList from './UserList';
 
 
 
+class App extends Component{
+  static defaultProps = {
+    store: {
+      participants: [],
+      chatEvents: []
+    }
+  };
 
-function App (props) {
-  
+  render(){
+  const {store} = this.props;
+
   return (
-    <p></p>
+      <div className="userList">
+        <UserList
+          key={store.participants.id}
+          avatar={store.participants.avatar}
+          name={store.participants.name}
+          onStage={store.participants.onStage}
+          />
+      </div>
   )
 }
-
+}
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -1,20 +1,39 @@
 import React from "react";
+import STORE from './store'
 
-
-
-
-function List(props) {
+function UserList(props) {
+  let onStage = [];
+  let inSession = [];
+  for(let i =0; i<STORE.participants.length; i++){
+    if(STORE.participants[i].onStage){
+      onStage.push(STORE.participants[i])
+    } else if (STORE.participants[i].inSession){
+      inSession.push(STORE.participants[i])
+    }
+  }
   
-  
-  return (
-    <div className="participant">
-      <img className="avatar" src={'props.avatar'} alt={'props.name'} />
-      <div>
-        <h5 className="user_name">{'props.name'}</h5>
-        <div className="status">{'status'}</div>
-      </div>
+
+  return(
+    <div className = 'ActiveList'>
+    <ul>
+      {onStage.map(onStage =>  
+      <li>
+        <img src={onStage.avatar} alt="onStage.name" />
+        <p>{onStage.name}</p> 
+        <p>On Stage</p>
+        </li>)}
+      </ul>
+
+      <ul>
+      {inSession.map(inSession =>  
+      <li>
+        <img src={inSession.avatar} alt="inSession.name" />
+        <p>{inSession.name}</p>
+         <p>In Session</p>
+         </li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default List;
+export default UserList;
