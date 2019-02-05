0bf38c412cd4b514c3fd72fcf92ed60008c2b386
Hunter Kreshock <hkreshock@gmail.com>
2019-02-05 13:23:11 -0700

    list pulls correct info

--- src/list.js
+++ src/list.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import store from './store';
 
 function List(props) {
-  const status = props.onStage ? "on stage" : "";
+  const status = store.participants[0].onStage ? "on stage" : "";
 
   return (
     <div className="participant">
-      <img className="avatar" src={props.avatar} alt={props.name} />
+      <img className="avatar" src={store.participants[0].avatar} alt={store.participants[0].name} />
       <div>
-        <h5 className="user_name">{props.name}</h5>
+        <h5 className="user_name">{store.participants[0].name}</h5>
         <div className="status">{status}</div>
       </div>
     </div>
